327c1b675fa58e705d3b9bb9cb860b0b1d3bc8bd
AyimanyMooncat <mooncatayimany@proton.me>
2025-05-15 21:25:33 -0600

    Add vector2d type

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 // This file is a prototype.
 
 #include "units.hh"
+#include <algorithm>
+#include <cstdint>
+#include <vector>
 
 // Only exposes literals
 // Does not cause any identifiers to breach their namespaces
@@ -29,9 +32,29 @@ struct cartesian2d {
 
 	// This is on purpose.
 	// I operate under encapsulation forbidding the use of protected fields
-   private:
+    private:
 	units::length::meter_t x_;
 	units::length::meter_t y_;
+};
+
+struct vector2d : public cartesian2d {
+	explicit constexpr vector2d(units::length::meter_t x_component,
+				    units::length::meter_t y_component) noexcept
+		: cartesian2d(x_component, y_component)
+	{
+	}
+
+	[[nodiscard]] constexpr auto square_magnitude() const noexcept
+		-> units::area::square_meter_t
+	{
+		return units::math::pow<2>(x()) + units::math::pow<2>(y());
+	}
+
+	[[nodiscard]] constexpr auto magnitude() const noexcept
+		-> units::length::meter_t
+	{
+		return units::math::sqrt(square_magnitude());
+	}
 };
 
 
