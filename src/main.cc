523fb0560ecba86decfb6109c7dde5fee6db5188
AyimanyMooncat <mooncatayimany@proton.me>
2025-05-15 21:25:53 -0600

    Add point2d type

--- src/main.cc
+++ src/main.cc
@@ -57,6 +57,47 @@ struct vector2d : public cartesian2d {
 	}
 };
 
+struct point2d : public cartesian2d {
+	explicit constexpr point2d(units::length::meter_t x_component,
+				   units::length::meter_t y_component) noexcept
+		: cartesian2d(x_component, y_component)
+	{
+	}
+
+	[[nodiscard]] constexpr auto
+	draw_vector_towards(point2d const &other) const noexcept -> vector2d
+	{
+		return vector2d{ other.x() - x(), other.y() - y() };
+	}
+
+	[[nodiscard]] constexpr auto
+	squared_distance_to(point2d const &other) const noexcept
+		-> units::area::square_meter_t
+	{
+		return draw_vector_towards(other).square_magnitude();
+	}
+
+	[[nodiscard]] constexpr auto
+	distance_to(point2d const &other) const noexcept
+		-> units::length::meter_t
+	{
+		return units::math::sqrt(squared_distance_to(other));
+	}
+
+	[[nodiscard]] constexpr auto
+	displaced_by(vector2d const &displacement_vector) const noexcept
+		-> point2d
+	{
+		return point2d{ x() + displacement_vector.x(),
+				y() + displacement_vector.y() };
+	}
+};
+
+namespace constants
+{
+constexpr point2d origin = point2d(0.0_m, 0.0_m);
+}
+
 
 auto main() -> int
 {
